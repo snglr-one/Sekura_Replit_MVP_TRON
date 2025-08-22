dd9c966b96439eabc9b223ce4dd1895bd73be2ed
snglr-one <snglrgroup@gmail.com>
2025-08-22 00:58:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname  = dirname(__filename);
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`SEKURA server running on http://localhost:${PORT}`);
 });
